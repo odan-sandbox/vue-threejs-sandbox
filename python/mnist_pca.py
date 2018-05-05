0058eab1362ea60d80867e9f5bb91952c1bf8bc4
odan <odan3240@gmail.com>
2018-05-05 21:05:22 +0900

    fix mnist_pca.py

--- python/mnist_pca.py
+++ python/mnist_pca.py
@@ -5,6 +5,8 @@ import random
 from shutil import copyfileobj
 from pathlib import Path
 
+import numpy as np
+
 from six.moves import urllib
 from sklearn.datasets.base import get_data_home
 from sklearn.datasets import fetch_mldata
@@ -13,6 +15,8 @@ from PIL import Image
 from tqdm import tqdm
 
 # https://github.com/scikit-learn/scikit-learn/issues/8588#issuecomment-292707231
+
+
 def fetch_mnist(data_home=None):
     mnist_alternative_url = "https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat"
     data_home = get_data_home(data_home=data_home)
@@ -25,30 +29,31 @@ def fetch_mnist(data_home=None):
         with open(mnist_save_path, "wb") as matlab_file:
             copyfileobj(mnist_url, matlab_file)
 
+def f(vec, feature_range=(-1, 1)):
+    min_ = min(vec)
+    max_ = max(vec)
+    ret = [(x - min_) / (max_ - min_) for x in vec]
+    ret = [2 * x - 1 for x in ret]
+    return np.array(ret)
 
 def main():
-    random.seed(42)
-    fetch_mnist()
-    mnist = fetch_mldata('MNIST original', data_home='data')
-
-    pca = PCA(n_components=3)
-    pca_vec = pca.fit_transform(mnist.data / 255)
-
-    N = mnist.data.shape[0]
-    images_dir = Path('data/images')
-    images_dir.mkdir(exist_ok=True)
-
     data = []
-    for i in tqdm(range(N)):
-        path = str(images_dir / '{:05}.bmp'.format(i))
-        Image.fromarray(mnist.data[i].reshape(28, 28)).save(path)
+    indexes = np.random.permutation(70000)[:1000]
+
+    # 10 x 100 だけ画像を並べる
+    sprite = Image.new('L', (28 * 10, 28 * 100))
+
+    for cnt, i in tqdm(enumerate(indexes)):
+        image = mnist.data[i].reshape(28, 28)
+        box = (28 * (cnt // 10), 28 * (cnt % 100))
+        sprite.paste(Image.fromarray(image), box)
         data.append({
             'vector': pca_vec[i].tolist(),
-            'path': path,
-            'label': mnist.target[i]
+            'box': box,
+            'label': mnist.target[i],
         })
 
-    random.shuffle(data)
+    sprite.save('data/mnist_sprite.bmp')
     Path('data/mnist_pca.json').write_text(json.dumps(data))
 
 
